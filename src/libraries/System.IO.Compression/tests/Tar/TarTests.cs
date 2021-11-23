27b47588439c06babcab0e2b5b9c503402df6b1e
carlossanlop <carlossanlop@users.noreply.github.com>
2021-11-22 18:36:46 -0800

    tests: Move manual asset creation tests to separate class.

--- src/libraries/System.IO.Compression/tests/Tar/TarTests.cs
+++ src/libraries/System.IO.Compression/tests/Tar/TarTests.cs
@@ -33,21 +33,6 @@ namespace System.IO.Compression.Tests
                 GetTarFile(CompressionMethod.Uncompressed, TarFormat.V7, testCaseName),
                 Path.Join(Directory.GetCurrentDirectory(), GetTestCaseFolderName(testCaseName)));
 
-        // Workaround for 'Read_Uncompressed_V7_Links'
-        [Theory]
-        [InlineData("file_hardlink")]
-        [InlineData("file_symlink")]
-        [InlineData("foldersymlink_folder_subfolder_file")]
-        public void Read_Uncompressed_V7_Links_ManuallyCreated(string testCaseName)
-        {
-            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
-
-            VerifyTarFileContents(
-                CompressionMethod.Uncompressed,
-                GetTarFile(CompressionMethod.Uncompressed, TarFormat.V7, testCaseName),
-                tmpDir.Path);
-        }
-
         #endregion
 
         #region V7 GZip
@@ -73,21 +58,6 @@ namespace System.IO.Compression.Tests
                 CompressionMethod.GZip,
                 GetTarFile(CompressionMethod.GZip, TarFormat.V7, testCaseName),
                 Path.Join(Directory.GetCurrentDirectory(), GetTestCaseFolderName(testCaseName)));
-
-        // Workaround for 'Read_Gzip_V7_Links'
-        [Theory]
-        [InlineData("file_hardlink")]
-        [InlineData("file_symlink")]
-        [InlineData("foldersymlink_folder_subfolder_file")]
-        public void Read_Gzip_V7_Links_ManuallyCreated(string testCaseName)
-        {
-            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
-
-            VerifyTarFileContents(
-                CompressionMethod.GZip,
-                GetTarFile(CompressionMethod.GZip, TarFormat.V7, testCaseName),
-                tmpDir.Path);
-        }
 
         #endregion
 
@@ -115,21 +85,6 @@ namespace System.IO.Compression.Tests
                 GetTarFile(CompressionMethod.Uncompressed, TarFormat.Ustar, testCaseName),
                 Path.Join(Directory.GetCurrentDirectory(), GetTestCaseFolderName(testCaseName)));
 
-        // Workaround for 'Read_Uncompressed_Ustar_Links'
-        [Theory]
-        [InlineData("file_hardlink")]
-        [InlineData("file_symlink")]
-        [InlineData("foldersymlink_folder_subfolder_file")]
-        public void Read_Uncompressed_Ustar_Links_ManuallyCreated(string testCaseName)
-        {
-            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
-
-            VerifyTarFileContents(
-                CompressionMethod.Uncompressed,
-                GetTarFile(CompressionMethod.Uncompressed, TarFormat.Ustar, testCaseName),
-                tmpDir.Path);
-        }
-
         #endregion
 
         #region Ustar GZip
@@ -156,32 +111,17 @@ namespace System.IO.Compression.Tests
                 GetTarFile(CompressionMethod.GZip, TarFormat.Ustar, testCaseName),
                 Path.Join(Directory.GetCurrentDirectory(), GetTestCaseFolderName(testCaseName)));
 
-        // Workaround for 'Read_Gzip_Ustar_Links'
-        [Theory]
-        [InlineData("file_hardlink")]
-        [InlineData("file_symlink")]
-        [InlineData("foldersymlink_folder_subfolder_file")]
-        public void Read_Gzip_Ustar_Links_ManuallyCreated(string testCaseName)
-        {
-            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
-
-            VerifyTarFileContents(
-                CompressionMethod.GZip,
-                GetTarFile(CompressionMethod.GZip, TarFormat.Ustar, testCaseName),
-                tmpDir.Path);
-        }
-
         #endregion
 
         #region Helpers
 
-        private enum CompressionMethod
+        protected enum CompressionMethod
         {
             Uncompressed,
             GZip,
         }
 
-        private static string GetTarFile(CompressionMethod compressionMethod, TarFormat format, string testCaseName)
+        protected static string GetTarFile(CompressionMethod compressionMethod, TarFormat format, string testCaseName)
         {
             (string compressionMethodFolder, string fileExtension) = compressionMethod switch
             {
@@ -197,74 +137,8 @@ namespace System.IO.Compression.Tests
 
         private static string GetTestCaseFolderName(string testCaseName) => Path.Join("TarTestData", "unarchived", testCaseName);
 
-        private TempDirectory GenerateExpectedLinkFilesAndFolders(string testCaseName)
-        {
-            var tmpDir = new TempDirectory();
-            switch (testCaseName)
-            {
-                case "file_hardlink":
-                    GenerateHardLinkTestAssets(tmpDir);
-                    break;
-                case "file_symlink":
-                    GenerateSymlinkTestAssets(tmpDir);
-                    break;
-                case "foldersymlink_folder_subfolder_file":
-                    GenerateFolderSymlinkTestAssets(tmpDir);
-                    break;
-                default:
-                    throw new NotSupportedException($"Unsupported test case: {testCaseName}");
-            }
-            return tmpDir;
-        }
-
-        // Generates a file and folder structure like the one from the 'file_hardlink' test case.
-        private void GenerateHardLinkTestAssets(TempDirectory tmpDir)
-        {
-            string targetPath = Path.Join(tmpDir.Path, "file.txt");
-            GenerateFileAsset(targetPath);
-
-            string linkPath = Path.Join(tmpDir.Path, "hardlink.txt");
-
-            CreateHardLink(linkPath, targetPath);
-        }
-
-        // Generates a file and folder structure like the one from the 'file_symlink' test case.
-        private void GenerateSymlinkTestAssets(TempDirectory tmpDir)
-        {
-            string targetPath = Path.Join(tmpDir.Path, "file.txt");
-            GenerateFileAsset(targetPath);
-
-            string linkPath = Path.Join(tmpDir.Path, "link.txt");
-            File.CreateSymbolicLink(linkPath, targetPath);
-        }
-
-        // Creates a file in the specified location.
-        private void GenerateFileAsset(string filePath)
-        {
-            using (StreamWriter writer = File.CreateText(filePath))
-            {
-                writer.Write("Hello world!");
-            }
-        }
-
-        // Generates a file and folder structure like the one from the 'foldersymlink_folder_subfolder_file' test case.
-        private void GenerateFolderSymlinkTestAssets(TempDirectory tmpDir)
-        {
-            string parentPath = Path.Join(tmpDir.Path, "parent");
-            Directory.CreateDirectory(parentPath);
-
-            string childPath = Path.Join(parentPath, "child");
-            Directory.CreateDirectory(childPath);
-
-            string filePath = Path.Join(childPath, "file.txt");
-            GenerateFileAsset(filePath);
-
-            string linkPath = Path.Join(tmpDir.Path, "childlink");
-            Directory.CreateSymbolicLink(linkPath, childPath);
-        }
-
         // Opens the specified tar file as a stream, decompresses it if necessary, then verifies the contents.
-        private void VerifyTarFileContents(CompressionMethod compressionMethod, string tarFilePath, string expectedFilesDir)
+        protected void VerifyTarFileContents(CompressionMethod compressionMethod, string tarFilePath, string expectedFilesDir)
         {
             using FileStream fs = File.Open(tarFilePath, FileMode.Open);
 
---
+++ src/libraries/System.IO.Compression/tests/Tar/TarTests_ManuallyCreatedAssets.Windows.cs
@@ -0,0 +1,140 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace System.IO.Compression.Tests
+{
+    // TODO: Make this class run in many platforms, not just Windows
+    public partial class TarTests_ManuallyCreatedAssets : TarTests
+    {
+        // Workaround for 'Read_Uncompressed_V7_Links'
+        [Theory]
+        [InlineData("file_hardlink")]
+        [InlineData("file_symlink")]
+        [InlineData("foldersymlink_folder_subfolder_file")]
+        public void Read_Uncompressed_V7_Links_ManuallyCreated(string testCaseName)
+        {
+            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
+
+            VerifyTarFileContents(
+                CompressionMethod.Uncompressed,
+                GetTarFile(CompressionMethod.Uncompressed, TarFormat.V7, testCaseName),
+                tmpDir.Path);
+        }
+
+        // Workaround for 'Read_Gzip_V7_Links'
+        [Theory]
+        [InlineData("file_hardlink")]
+        [InlineData("file_symlink")]
+        [InlineData("foldersymlink_folder_subfolder_file")]
+        public void Read_Gzip_V7_Links_ManuallyCreated(string testCaseName)
+        {
+            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
+
+            VerifyTarFileContents(
+                CompressionMethod.GZip,
+                GetTarFile(CompressionMethod.GZip, TarFormat.V7, testCaseName),
+                tmpDir.Path);
+        }
+
+
+        // Workaround for 'Read_Uncompressed_Ustar_Links'
+        [Theory]
+        [InlineData("file_hardlink")]
+        [InlineData("file_symlink")]
+        [InlineData("foldersymlink_folder_subfolder_file")]
+        public void Read_Uncompressed_Ustar_Links_ManuallyCreated(string testCaseName)
+        {
+            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
+
+            VerifyTarFileContents(
+                CompressionMethod.Uncompressed,
+                GetTarFile(CompressionMethod.Uncompressed, TarFormat.Ustar, testCaseName),
+                tmpDir.Path);
+        }
+
+        // Workaround for 'Read_Gzip_Ustar_Links'
+        [Theory]
+        [InlineData("file_hardlink")]
+        [InlineData("file_symlink")]
+        [InlineData("foldersymlink_folder_subfolder_file")]
+        public void Read_Gzip_Ustar_Links_ManuallyCreated(string testCaseName)
+        {
+            using TempDirectory tmpDir = GenerateExpectedLinkFilesAndFolders(testCaseName);
+
+            VerifyTarFileContents(
+                CompressionMethod.GZip,
+                GetTarFile(CompressionMethod.GZip, TarFormat.Ustar, testCaseName),
+                tmpDir.Path);
+        }
+
+        private TempDirectory GenerateExpectedLinkFilesAndFolders(string testCaseName)
+        {
+            var tmpDir = new TempDirectory();
+            switch (testCaseName)
+            {
+                case "file_hardlink":
+                    GenerateHardLinkTestAssets(tmpDir);
+                    break;
+                case "file_symlink":
+                    GenerateSymlinkTestAssets(tmpDir);
+                    break;
+                case "foldersymlink_folder_subfolder_file":
+                    GenerateFolderSymlinkTestAssets(tmpDir);
+                    break;
+                default:
+                    throw new NotSupportedException($"Unsupported test case: {testCaseName}");
+            }
+            return tmpDir;
+        }
+
+        // Generates a file and folder structure like the one from the 'file_hardlink' test case.
+        private void GenerateHardLinkTestAssets(TempDirectory tmpDir)
+        {
+            string targetPath = Path.Join(tmpDir.Path, "file.txt");
+            GenerateFileAsset(targetPath);
+
+            string linkPath = Path.Join(tmpDir.Path, "hardlink.txt");
+
+            CreateHardLink(linkPath, targetPath);
+        }
+
+        // Generates a file and folder structure like the one from the 'file_symlink' test case.
+        private void GenerateSymlinkTestAssets(TempDirectory tmpDir)
+        {
+            string targetPath = Path.Join(tmpDir.Path, "file.txt");
+            GenerateFileAsset(targetPath);
+
+            string linkPath = Path.Join(tmpDir.Path, "link.txt");
+            File.CreateSymbolicLink(linkPath, targetPath);
+        }
+
+        // Creates a file in the specified location.
+        private void GenerateFileAsset(string filePath)
+        {
+            using (StreamWriter writer = File.CreateText(filePath))
+            {
+                writer.Write("Hello world!");
+            }
+        }
+
+        // Generates a file and folder structure like the one from the 'foldersymlink_folder_subfolder_file' test case.
+        private void GenerateFolderSymlinkTestAssets(TempDirectory tmpDir)
+        {
+            string parentPath = Path.Join(tmpDir.Path, "parent");
+            Directory.CreateDirectory(parentPath);
+
+            string childPath = Path.Join(parentPath, "child");
+            Directory.CreateDirectory(childPath);
+
+            string filePath = Path.Join(childPath, "file.txt");
+            GenerateFileAsset(filePath);
+
+            string linkPath = Path.Join(tmpDir.Path, "childlink");
+            Directory.CreateSymbolicLink(linkPath, childPath);
+        }
+
+        private void CreateHardLink(string linkPath, string targetPath) => Interop.Kernel32.CreateHardLink(linkPath, targetPath);
+    }
+}
